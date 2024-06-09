ef3c48d5869a9e41b340324fe94437af7bf61aa4
Suleman <34047751+zsulley58@users.noreply.github.com>
2024-06-09 07:49:01 +0000

    First Sidebar Component commit

--- plant-performance-dashboard/src/components/Sidebar.js
+++ plant-performance-dashboard/src/components/Sidebar.js
@@ -7,6 +7,14 @@ function Sidebar() {
       <div>Inlet Particulate Filter</div>
       <div>Outlet Coalescer Filter</div>
       <div>Compressors</div>
+      <div className="view-filters">
+        <h3>View Filters</h3>
+        <button>Day</button>
+        <button>Week</button>
+        <button>Month</button>
+        <button>Quarter</button>
+        <button>Year</button>
+      </div>
     </div>
   );
 }
